2c4d211a7c93ced7002116c84ac0d03556d1f058
Hyeonseok Choi <worldneedscat@gmail.com>
2019-04-15 01:43:17 +0900

    Implement rotation, fixing unexpected behaviors

--- Homework02/hw02.py
+++ Homework02/hw02.py
@@ -5,20 +5,14 @@ from OpenGL.GLUT import *
 
 window = 0
 width, height = 1280, 720
+radius = min(width, height)/2
 oldx, oldy, mousepressed = 0, 0, False
+
+#for debug
+counter = 0
 
 poscam = [0, 4.4, 4.4]
 degree, znear, zfar = 90.0, 0.1, 300
-
-
-def doMouse(button, state, x, y):
-    global mousepressed
-    if button == GLUT_LEFT_BUTTON:
-        if state == GLUT_DOWN:
-            mousepressed = True
-        elif state == GLUT_UP:
-            mousepressed = False
-    glutPostRedisplay()
 
 def doKeyboard(*args):
     global poscam, degree
@@ -35,15 +29,98 @@ def doKeyboard(*args):
             poscam[i] /= 1.1
     elif ch == 'c':
         degree *= 1.1
-        print('degree: ', degree)
     elif ch == 'v':
         degree /= 1.1
-        print('degree: ', degree)
 
     glutPostRedisplay()
 
+def normalize(x, y):
+    global width, height, radius
+    x, y = x - width/2, y - height/2
+    #print('translated coordinate: ', x, y)
+    if x**2 + y**2 > radius*radius:
+        x, y = x/(math.sqrt(x**2 + y**2)), y/(math.sqrt(x**2 + y**2))
+        return x, y, 0
+    else:
+        x, y = x/radius, y/radius
+        return x, y, math.sqrt(1 - x**2 - y**2)
+
+def crossproduct(u, v):
+    return u[1]*v[2]-u[2]*v[1], u[2]*v[0]-u[0]*v[2], u[0]*v[1]-u[1]*v[0]
+
+def dotproduct(u, v):
+    dp = 0
+    for i in range(len(u)):
+        dp += u[i]*v[i]
+    return dp
+
+def length(a):
+    return math.sqrt(dotproduct(a, a))
+
+def getangle(u, v):
+    return math.acos(dotproduct(u, v)/(length(u)*length(v)))
+
+def rotatequaternion(u, v):
+    real = [math.cos(getangle(u, v)/2)]
+    imag = [num*math.sin(getangle(u, v)/2) for num in crossproduct(u, v)]
+    real.extend(imag)
+    return real
+
+def qtproduct(a, b):
+    return [a[0]*b[0]-a[1]*b[1]-a[2]*b[2]-a[3]*b[3],
+            a[0]*b[1]+a[1]*b[0]+a[2]*b[3]-a[3]*b[2],
+            a[0]*b[2]-a[1]*b[3]+a[2]*b[0]+a[3]*b[1],
+            a[0]*b[3]+a[1]*b[2]-a[2]*b[1]+a[3]*b[0]
+            ]
+
+def qtconjugate(a):
+    return [a[0], -a[1], -a[2], -a[3]]
+
+def rotatecamera(camera, x, y):
+    global oldx, oldy, poscam
+    poscamlen = length(poscam)
+    v1 = normalize(oldx, oldy)
+    v2 = normalize(x, y)
+    axis = crossproduct(v1, v2)
+    theta = getangle(v1, v2)
+    # reverse angle
+    qtinv = rotatequaternion(v1, v2)
+    print(qtinv)
+    qt = qtconjugate(qtinv)
+    camvec = [0]
+    camvec.extend(camera)
+    newqt = qtproduct(qtproduct(qt, camvec), qtinv)
+    newpos = newqt[1:4]
+    poscam = [num/length(newpos)*poscamlen for num in newpos]
+
+def doMouse(button, state, x, y):
+    global mousepressed, oldx, oldy
+    if button == GLUT_LEFT_BUTTON:
+        if state == GLUT_DOWN:
+            mousepressed = True
+            oldx, oldy = x, y
+            #print('original coordinate: ', x, y)
+            #print('normalized coordinate: ', normalize(x, y))
+        elif state == GLUT_UP:
+            mousepressed = False
+            #print('original coordinate: ', x, y)
+            #print('normalized coordinate: ', normalize(x, y))
+            oldx, oldy = x, y
+    glutPostRedisplay()
+
 def doMotion(x, y):
-    pass
+    global counter
+    if counter == 10:
+        counter = 0
+        print('theta: ', getangle(normalize(oldx, oldy), normalize(x, y))/math.pi*180)
+        rotatecamera(poscam, x, y)
+    else:
+        counter += 1
+
+def doPassiveMotion(x, y):
+    global oldx, oldy
+    oldx, oldy = x, y
+    glutPostRedisplay()
     
 
 def drawcube():
@@ -92,5 +169,6 @@ if __name__ == '__main__':
     glutKeyboardFunc(doKeyboard)
     glutMouseFunc(doMouse)
     glutMotionFunc(doMotion)
+    glutPassiveMotionFunc(doPassiveMotion)
     glutIdleFunc(draw)
     glutMainLoop()
